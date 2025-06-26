599f9f5337757162f684be3f0187737a8cbe3e81
Gavin Ogren <72085418+G-SCRAP@users.noreply.github.com>
2025-06-26 11:24:20 -0700

    Initial Commit

---
+++ habits-app/habits_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  habits_appApp.swift
+//  habits-app
+//
+//  Created by Gavin  Ogren on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct habits_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ habits-appTests/habits_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  habits_appTests.swift
+//  habits-appTests
+//
+//  Created by Gavin  Ogren on 6/26/25.
+//
+
+import Testing
+@testable import habits_app
+
+struct habits_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
